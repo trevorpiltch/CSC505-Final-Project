04e6a343549e7d4bcbbaa68b656e1e92645b5f9b
Trevor <trevor@piltch.com>
2021-03-10 12:33:06 -0500

    Added leafNode class. Didn't push since waiting for Eric to do it.

--- src/HTLeafNode.java
+++ src/HTLeafNode.java
@@ -1,2 +1,25 @@
-public class HTLeafNode {
+public class HTLeafNode<E> {
+    private E value;
+    private int frequency;
+
+    public HTLeafNode(E val) {
+        value = val;
+        frequency = 0;
+    }
+
+    public E getValue() {
+        return value;
+    }
+
+    public void setValue(E value) {
+        this.value = value;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
 }
