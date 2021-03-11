c7c0d8ccf7f5c1e8300c440c5075b28e4b1fe093
Trevor <trevor@piltch.com>
2021-03-11 08:53:08 -0500

    Saving leaf node class.

--- src/Tester.java
+++ src/Tester.java
@@ -1,9 +1,57 @@
+import java.util.ArrayList;
+import java.util.Dictionary;
+
 public class Tester {
     public static void main(String[] args) {
         System.out.println("Hello Eric");
         HTInternalNode node = new HTInternalNode();
+        char firstChar;
+        String value = "eebbeecdebeeebecceeeddebbbeceedebeeddeeeecceeeedeeedeeebeedeceedebeeedeceeedebee";
 
-        node.setValue(2);
-        System.out.println(node.getValue());
+        firstChar = value.toCharArray()[0];
+
+//        while (firstChar != null) {
+
+            System.out.println(findFrequencies(value));
+//        }
+
+    }
+
+    public static Dictionary<Character, Integer> findFrequency(char searchValue, String value ) {
+        int frequency = 0;
+        char[] charArray = new char[value.length()];
+        charArray = value.toCharArray();
+        Dictionary<Character, Integer> returnData = null;
+
+        for (int i = 0; i < charArray.length; i ++) {
+            if (charArray[i] == searchValue) {
+                frequency += 1;
+            }
+        }
+
+        returnData.put((Character) searchValue, frequency);
+
+        return returnData;
+    }
+
+    public static Dictionary<Character, Integer>[] findFrequencies(String value) {
+        ArrayList<Character> chars = new ArrayList<Character>();
+
+        char[] inputChars = value.toCharArray();
+
+        char firstChar = inputChars[0];
+
+        Dictionary<Character, Integer> returnData = new Dictionary<Character, Integer>;
+
+        //Put all the characters in inputChars into the ArrayList (so they can be removed later)
+        for (char c : inputChars) {
+            chars.add(c);
+        }
+
+        while (!chars.isEmpty()) {
+            returnData.put(firstChar, findFrequency(firstChar, value).);
+        }
     }
 }
+
+
